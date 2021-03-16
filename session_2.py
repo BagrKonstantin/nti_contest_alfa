116c4d0bd596396c859795f825b19a599b8b141a
Abusswalker <af55748@gmail.com>
2021-03-16 23:46:59 +0300

    нарисовал

--- session_2.py
+++ session_2.py
@@ -17,12 +17,40 @@ class UI_Task1(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle('enter data')
         self.path = path
-
+        self.update_data()
+        self.update_table()
         self.tableWidget_2.cellClicked.connect(self.open_tab)
-        self.tableWidget_2.setRowCount(1)
 
-        self.tableWidget_2.setItem(0, 0, QTableWidgetItem())
-        self.tableWidget_2.item(0, 0).setText("  sdg")
+    def update_table(self):
+        try:
+            self.tableWidget_2.setRowCount(len(self.data))
+            statuses = ['Новый', 'В работе', 'Комплект без оригинала', 'Полный комплект']
+            stadias = ['Новый', 'На рассмотрении', 'Отправлено на доработку', 'Принято']
+
+            for i in range(len(self.data)):
+                print(self.data[i])
+                self.tableWidget_2.setItem(i, 0, QTableWidgetItem())
+                self.tableWidget_2.setItem(i, 1, QTableWidgetItem())
+                self.tableWidget_2.setItem(i, 2, QTableWidgetItem())
+                self.tableWidget_2.setItem(i, 3, QTableWidgetItem())
+                self.tableWidget_2.setItem(i, 4, QTableWidgetItem())
+                self.tableWidget_2.setItem(i, 5, QTableWidgetItem())
+                self.tableWidget_2.item(i, 0).setText(str(self.data[i][1] + " " + self.data[i][2] + " " + self.data[i][3]))
+                self.tableWidget_2.item(i, 1).setText(str(self.data[i][4]))
+                self.tableWidget_2.item(i, 2).setText(str(self.data[i][5]))
+                self.tableWidget_2.item(i, 3).setText(str(self.data[i][8]))
+                self.tableWidget_2.item(i, 4).setText(str(statuses[self.data[i][-2]]))
+                self.tableWidget_2.item(i, 5).setText(str(stadias[self.data[i][-1]]))
+        except Exception as err:
+            print(err)
+
+
+
+    def update_data(self):
+        con = sqlite3.connect(self.path)
+        cur = con.cursor()
+        self.data = cur.execute("""Select * from user""").fetchall()
+        con.close()
 
 
     def open_tab(self):
@@ -42,7 +70,8 @@ class UI_Task1(QMainWindow, Ui_MainWindow):
         if msg.clickedButton() == ok_button:
             print("opendialog")
             try:
-                dialog = Dialog(self, num=self.tableWidget_2.verticalHeader().sortIndicatorSection())
+                print(self.tableWidget_2.verticalHeader().sortIndicatorSection())
+                dialog = Dialog(self, self.data[self.tableWidget_2.verticalHeader().sortIndicatorSection() - 1])
                 dialog.show()
                 print("no error")
             except Exception as error:
@@ -51,12 +80,14 @@ class UI_Task1(QMainWindow, Ui_MainWindow):
 
 
 class Dialog(QMainWindow, Dialog_ui):
-    def __init__(self, parent=None, num=None):
+    def __init__(self, parent=None, *args):
         parent.setDisabled(True)
         self.parent = parent
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('Dialog')
+
+
 
 
         self.pushButton.clicked.connect(self.back)
@@ -86,6 +117,6 @@ if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    mainWindow = UI_Task1("data/bd")
+    mainWindow = UI_Task1("bd.db")
     mainWindow.show()
     sys.exit(app.exec())
