f9ab074c65fbdd8a5dfe9e9e93553ba3419193ca
BKonstantin <costa.spy@yandex.ru>
2021-03-17 08:36:35 +0300

    first nah

--- session_2.py
+++ session_2.py
@@ -21,6 +21,14 @@ class UI_Task1(QMainWindow, Ui_MainWindow):
         self.update_table()
         self.tableWidget_2.cellClicked.connect(self.open_tab)
 
+        self.tableWidget_2.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        self.tableWidget_2.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        self.tableWidget_2.horizontalHeader().setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        self.tableWidget_2.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
+        self.tableWidget_2.horizontalHeader().setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)
+        self.tableWidget_2.horizontalHeader().setSectionResizeMode(5, QtWidgets.QHeaderView.ResizeToContents)
+
+
     def update_table(self):
         try:
             self.tableWidget_2.setRowCount(len(self.data))
@@ -36,7 +44,7 @@ class UI_Task1(QMainWindow, Ui_MainWindow):
                 self.tableWidget_2.setItem(i, 4, QTableWidgetItem())
                 self.tableWidget_2.setItem(i, 5, QTableWidgetItem())
                 self.tableWidget_2.item(i, 0).setText(str(self.data[i][1] + " " + self.data[i][2] + " " + self.data[i][3]))
-                self.tableWidget_2.item(i, 1).setText(str(self.data[i][4]))
+                self.tableWidget_2.item(i, 1).setText("Мужской" if self.data[i][4] == 0 else "Женский")
                 self.tableWidget_2.item(i, 2).setText(str(self.data[i][5]))
                 self.tableWidget_2.item(i, 3).setText(str(self.data[i][8]))
                 self.tableWidget_2.item(i, 4).setText(str(statuses[self.data[i][-2]]))
@@ -86,6 +94,40 @@ class Dialog(QMainWindow, Dialog_ui):
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('Dialog')
+
+        for i, j in enumerate(args[0]):
+            print(j, i)
+
+        data = args[0]
+
+        self.lineEdit_name_2.setText(str(data[1]))
+        self.lineEdit_secondname_2.setText(str(data[2]))
+        self.lineEdit_surname_2.setText(str(data[3]))
+        self.lineEdit_sex_2.setText("Мужской" if data[4] == 0 else "Женский")
+        self.lineEdit_email_2.setText(str(data[5]))
+        # password
+        self.lineEdit_birht_date_2.setText(str(data[7]))
+        self.lineEdit_phone_2.setText(str(data[8]))
+        self.lineEdit_place_birth_2.setText(str(data[9]))
+        self.radioButton_hostel_2.setChecked(True if data[10] else False)
+        # фото
+        self.lineEdit_series_2.setText(str(data[12]))
+        self.lineEdit_pass_number_2.setText(str(data[13]))
+        self.lineEdit_given_by_2.setText(str(data[14]))
+        self.lineEdit_code_2.setText(str(data[15]))
+        self.lineEdit_given_date_2.setText(str(data[16]))
+        # фото паспорт
+        self.lineEdit_adress_2.setText(str(data[18]))
+        # фото согласие
+        self.lineEdit_number_of_doc_frame_3.setText(str(data[20]))
+        # фото аттестат
+        self.lineEdit_teach_form.setText("бюджет" if data[22] else "платное")
+        self.lineEdit_rus_2.setText(str(data[23]))
+        self.lineEdit_math_2.setText(str(data[24]))
+        self.lineEdit_inf.setText(str(data[25]))
+        self.lineEdit_fizika.setText(str(data[26]))
+        self.lineEdit_achiev.setText(str(data[27]))
+        # фото достижение
 
 
 
